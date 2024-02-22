e7a3559e8cc340eefda65164b66625b2d3b62e4d
anaezquerro <anaezquerro0701@gmail.com>
2024-02-22 08:35:08 +0100

    preparing stream learning part

---
+++ project/stream.py
@@ -0,0 +1,81 @@
+from typing import Dict, Union 
+import numpy as np 
+from datetime import datetime 
+from river.compose import Pipeline, Discard, FuncTransformer, TargetTransformRegressor
+from river.preprocessing import StandardScaler
+import plotly.graph_objects as go
+
+
+
+def train(data, models: Dict[str, Pipeline]) -> Dict[str, np.ndarray]:
+    preds = {name: [] for name in models.keys()}
+    for x, y in data:
+        for name, model in models.items():
+            preds[name].append(model.predict_one(x))
+            model.learn_one(x, y)
+    return {name: np.array(pred) for name, pred in preds.items()}
+
+def plot_preds(preds: Dict[str, np.ndarray], target: np.ndarray, metrics: Dict[str, float]):
+    fig = go.Figure() 
+    fig.add_trace(go.Scatter(y=target, name='Temperature'))
+    for name in preds.keys():
+        fig.add_trace(go.Scatter(y=preds[name], name=f'{name} ({metrics[name]:.2})'))
+    fig.update_layout(width=1000, height=600, title_text='Temperature', template='seaborn')
+    return fig
+
+
+def get_date_features(x: Dict[str, str]) -> Dict[str, float]:
+    month = int(x['Date'].split("/")[1])
+    season = ((month-1) % 12)//4
+
+    time_format = "%H:%M:%S"
+    datetime_object = datetime.strptime(x['Time'], time_format)
+
+    hour_of_day = datetime_object.hour
+
+    time_cos = np.cos(hour_of_day)
+    time_sin = np.sin(hour_of_day)
+    
+    # cast to float 
+    x.update({'month':month, 'season':season, 'time_cos': time_cos, 'time_sin':time_sin}) 
+    return x
+             
+             
+class SequentialImputer(object):
+    def __init__(self, detect = np.isnan, target: bool = False):
+        self.last = dict() if not target else 0
+        self.detect = detect
+        self.__class__.__name__ = 'SequentialImputer'
+        
+    
+    def __call__(self, x: Union[Dict[str, float], float]):
+        if isinstance(x, dict):
+            x = {feat: self.last[feat] if self.detect(value) else value for feat, value in x.items()}
+        else:
+            x = self.last if self.detect(x) else x
+        self.last = x 
+        return x
+    
+    @property
+    def __name__(self):
+        return 'SequentialImputer'
+    
+
+def cast_float(x: str):
+    try: 
+        return float(x)
+    except ValueError:
+        return x
+    
+    
+
+def add_preprocess(model: Pipeline, use_time: bool = True, impute: bool = True) -> Pipeline:
+    steps = []
+    if use_time:
+        steps.append(FuncTransformer(get_date_features))
+    steps.append(Discard('Time', 'Date'))
+    if impute:
+        steps.append(FuncTransformer(SequentialImputer(lambda x: x == -200)))
+        imp = SequentialImputer(lambda x: x == -200, target=True)
+        model = TargetTransformRegressor(model, func=imp, inverse_func=imp)
+    return Pipeline(*steps, StandardScaler(), model)
