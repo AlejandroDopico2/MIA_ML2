b43836e6f398a7a22ba387394e3a4843896e9277
AbelJuncal <abel.juncal@udc.es>
2024-03-13 10:30:05 +0100

    Add U4 and client/server

---
+++ flowerElements/client.py
@@ -0,0 +1,9 @@
+from utils import *
+
+# Start the client
+model=generate_ann()
+
+#The following line is deprecated in the current flower version
+# fl.client.start_numpy_client(server_address="localhost:8080", client=MyClient(model,trainloaders[0],valloaders[0]))
+
+fl.client.start_client(server_address=f"localhost:8080", client=MyClient(model,trainloaders[0],valloaders[0]).to_client())
